2d599bd5ac7e43799bb905aa9e416a32a58e3101
Lisowsk1 <szymon.lisowski.it@gmail.com>
2025-03-14 22:50:08 +0100

    a

--- src/dsaa/lab02/Document.java
+++ src/dsaa/lab02/Document.java
@@ -2,24 +2,24 @@ package dsaa.lab02;
 
 import java.util.Scanner;
 
-public class Document {
-    public static void loadDocument(String name, Scanner scan) {
-        while(scan.hasNextLine()) {
-            String line = scan.nextLine().toLowerCase();
+public class Document{
+	public String name;
+	public OneWayLinkedList<Link> links;
+	public Document(String name, Scanner scan) {
+			// TODO
+			load(scan);
+		}
+	public void load(Scanner scan) {
+		//TODO
+	}
+	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
+	private static boolean correctLink(String link) {
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
 
-            if(line.equals("eod"))
-                break;
-
-            String word[] = line.split(" ");
-            for (int i = 0; i < word.length; i++) {
-                if(correctLink(word[i]))
-                    System.out.println(word[i].substring(5));
-            }
-        }
-    }
-
-    // accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-    public static boolean correctLink(String link) {
-        return link.matches("link=[a-z]+[a-z0-9_]*$");
-    }
 }
